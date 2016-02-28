f743e068c090c1940d6a75f035fa5294b323ac19
bpeterson1663 <bpeterson1663@gmail.com>
2016-02-28 01:20:54 -0600

    Added delete functionality to it and subtracted from total montly salary

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var employees =[];
 var counter = 0;
+var totalMonthlyCost = 0;
 $(document).ready(function(){
   //listen for submit
   $('#employeeInfo').on('submit', function(event){
@@ -9,36 +10,45 @@ $(document).ready(function(){
       //cycle through each field and collect value to store in object
       $.each($("#employeeInfo").serializeArray(), function(i, field) {
           formValues[field.name] = field.value;
+
       });
+
       $('#employeeInfo').find('input[type=text]').val("");
-      employees.push(formValues);
-      console.log(employees);
+      employees.push(formValues);//pushes employees from form to array
+      //console.log(employees);
       calculateMonthly();
       appendEmpInfo();
-      counter++;
-      //appendEmpInfo();
+      counter++;//increase employee counter to be used as a reference to append just the next employee
       //push values to an array
       //reset values
   });
-  //calculateMonthly();
+  //Delete Each Employee by targeting all buttons in the #employeeData
+  $('#employeeData').on('click', 'button', function(){
+    var removeSalary = $(this).data('salary')/12; //Grabs the data-salary attribute and divideds by 12 for the montly salary to be removed
+    console.log(removeSalary);
+    $(this).closest('.employeeRow').remove();//grabs the closest class of employeeRow and removes that
+    totalMonthlyCost -= emoveSalary;//subtracts removes Salary from totalMontlyCost
+    $('#monthlysalarycost').text(totalMonthlyCost); //sets HTML text when employee is deleted
+  });
+  $('#monthlysalarycost').text(totalMonthlyCost);
   //End Submit button clicked
 });
 //calculate montly cost by cycling through employees array and adding that to a global variable
 function calculateMonthly(){
-  var totalMonthlyCost = 0;
-  for (var i = 0; i < employees.length; i++) {
+  for (var i = 0; i < employees.length; i++) { //creates a reference for each person in the array
     var employee = employees[i];
-    //$('#employeeData').append('<tr><td>' + employee.firstname + '</td></tr>');//<td>'+ employee.lastname + '</td><td>' + employee.employeenumber + '</td><td>' + employee.jobtitle + '</td><td>' + employee.yearlysalary + '</td></tr>');
-    totalMonthlyCost += parseInt(employee.yearlysalary)/12;
-    console.log(totalMonthlyCost);
-
   }
-  $('#monthlysalarycost').text(totalMonthlyCost);
+    totalMonthlyCost += Math.round(parseInt(employee.yearlysalary)/12);
+    //console.log(totalMonthlyCost);
+
+  $('#monthlysalarycost').text(totalMonthlyCost);//set HTML text to montly cost when employee is add
 
 }
 
 function appendEmpInfo(){
     var employee = employees[counter];
-    $('#employeeData').append('<tr><td>' + employee.firstname + '</td><td>'+ employee.lastname + '</td><td>' + employee.employeenumber + '</td><td>' + employee.jobtitle + '</td><td>' + employee.yearlysalary + '</td></tr>');
-
+    var salary = employee.yearlysalary;//grabs the employees Salary
+    var row = '<tr class="employeeRow"><td>' + employee.firstname + '</td><td>'+ employee.lastname + '</td><td>' + employee.employeenumber + '</td><td>' + employee.jobtitle + '</td><td class="salaryColumn">' + employee.yearlysalary + '</td><td><button data-salary="' + salary + '">Delete</button></td></tr>';
+    $('#employeeData').append(row);
+    $('.salaryColumn').data('salary', salary); //sets the data-salary attribute to the value of the employees salary
 }
